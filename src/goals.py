7e716ec28e43bc27194010547308a107c90366b1
Your Name <you@example.com>
2026-04-02 00:11:09 +0900

    目標設定ロジック実装

--- src/goals.py
+++ src/goals.py
@@ -18,19 +18,10 @@ class Goals(ctk.CTk):
          self.pulldown = {'未着手': -1, '進行中': 0, '完了': 1}
          self.button = ctk.CTkButton(self, text="登録", command=self.save)
          self.option1 = ctk.CTkOptionMenu(self,values=['未着手', '進行中', '完了'])
-         self.option2 = ctk.CTkOptionMenu(self,values=['未着手', '進行中', '完了'])
-         self.option3 = ctk.CTkOptionMenu(self,values=['未着手', '進行中', '完了'])
-         self.option4 = ctk.CTkOptionMenu(self,values=['未着手', '進行中', '完了'])
          self.update_button = ctk.CTkButton(self, text="更新", command=self.update)
 
          self.option1.set('未着手')
          self.option1.pack()
-         self.option2.set('未着手')
-         self.option2.pack()
-         self.option3.set('未着手')
-         self.option3.pack()
-         self.option4.set('未着手')
-         self.option4.pack()
 
          ctk.set_appearance_mode("light")
          ctk.set_default_color_theme("blue")
@@ -47,30 +38,32 @@ class Goals(ctk.CTk):
          self.entry1.pack(pady=10)
          self.entry2 = ctk.CTkEntry(self)
          self.entry2.pack(pady=10)
-         self.entry3 = ctk.CTkEntry(self)
-         self.entry3.pack(pady=10)
-         self.entry4 = ctk.CTkEntry(self)
-         self.entry4.pack(pady=10)
+         
          
     def save(self):
-        self.goals = {'key': self.json_date, "date": self.date, 
-                      'goal': [
-                          {"goals_1": self.entry1.get(), "states": -1},
-                          {"goals_2": self.entry2.get(), "states": -1},
-                          {"goals_3": self.entry3.get(), "states": -1},
-                          {"goals_4": self.entry4.get(), "states": -1}
-                          ]}
+        import uuid
+        
+        entry = [self.entry1, self.entry2]
 
-        if self.json_path is None:
-            print("環境変数が機能していません")
-        else:
-            full_path = os.path.join(self.json_path, self.json_file)
-    
-        with open(full_path, 'a', encoding='utf-8') as f:
-            data = json.dumps(self.goals, ensure_ascii=False)
-            f.write(data + "\n")
+        for i in entry:
+            if i.get() == "":
+                continue
+            recode_id = str(uuid.uuid4())
+            self.goals = {'key': recode_id,'created_at': self.json_date,"date": self.date, 
+                        'task': i.get(), "states": -1, "updated_at": None}
+
+            if self.json_path is None:
+                print("環境変数が機能していません")
+            else:
+                full_path = os.path.join(self.json_path, self.json_file)
+                
+            with open(full_path, 'a', encoding='utf-8') as f:
+                data = json.dumps(self.goals, ensure_ascii=False)
+                f.write(data + "\n")
 
     def update(self):
+        serch_task = None
+        serch_date = None
         update_data = []
         if self.json_path is None:
             print("環境変数が機能していません")
@@ -83,25 +76,29 @@ class Goals(ctk.CTk):
                     file_data = json.loads(data)
                 except:
                     continue
-                if file_data['key'] == self.json_date:
+                if file_data['states'] == -1 or file_data['states'] == 0:
                     print(file_data)
                     update_data.append(file_data)
                     print(update_data)
 
-        val_a = self.pulldown[self.option1.get()]
-        val_b = self.pulldown[self.option2.get()]
-        val_c = self.pulldown[self.option3.get()]
-        val_d = self.pulldown[self.option4.get()]
+        target = []
+        for i in update_data:
+            if i['created_at'] != self.json_date:
+                continue
+            target.append(i)
+        print(target)
+        if not target: return
+        serch_task = target[0]
+        serch_date = self.json_date
 
-        if val_a != -1:
-            update_data[0]['goal'][0]['states'] = val_a
-        if val_b != -1:
-            update_data[0]['goal'][1]['states'] = val_b
-        if  val_c != -1:
-            update_data[0]['goal'][2]['states'] = val_c
-        if val_d != -1:
-            update_data[0]['goal'][3]['states'] = val_d 
-        
+        val_a = self.pulldown[self.option1.get()]
+
+        for i in update_data:
+            if i['key'] == serch_task['key']:
+                i['states'] = val_a
+                if val_a == 1:
+                    i['updated_at'] = serch_date
+
         entry_data = []
         with open(full_path, 'r', encoding='utf-8')as f:
             print(len(update_data))
@@ -114,8 +111,8 @@ class Goals(ctk.CTk):
         
         with open(full_path, 'w') as f:
             for row in entry_data:
-                if row['key'] == self.json_date:
-                    row = update_data[0]
+                if row['key'] == serch_task['key']:
+                    row = serch_task
                 new_data = json.dumps(row, ensure_ascii=False)
                 f.write(new_data + '\n')
 
